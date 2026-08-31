039d5b70c043cd2e989a8f20be5aaa83e32fa87d
Usman9889 <usman9889a@gmail.com>
2026-08-31 16:23:51 +0530

    Second largest

--- Arrays/SecondLargest.cpp
+++ Arrays/SecondLargest.cpp
@@ -1,8 +1,8 @@
 //Brute force
+//Time Complexity: O(nlogn)
+//Space Complexity: O(1)
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int main() {
     int n;
     cin >> n;
@@ -20,4 +20,59 @@ int main() {
         }
         break;
     }
+}
+//Better Approach (Two Pass search)
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int n;
+    cin >> n;
+    vector < int > arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int Largest = -1, secondLargest = -1;
+    //Finding largest
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > Largest) {
+            Largest = arr[i];
+        }
+    }
+    //Finding second largest
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > secondLargest && arr[i] != Largest) {
+            secondLargest = arr[i];
+        }
+    }
+    cout << secondLargest;
+}
+
+//Optimal Approach
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int n;
+    cin >> n;
+    vector < int > arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int Largest = -1, secondLargest = -1;
+    //Finding largest
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > Largest) {
+            secondLargest = Largest;
+            Largest = arr[i];
+        }
+        else if(arr[i] < Largest && arr[i] > secondLargest) {
+            secondLargest = arr[i];
+        }
+    }
+    cout << secondLargest;
 }
