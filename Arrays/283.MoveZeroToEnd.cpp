a422e28ceabaf12bb33823dd10c53fa3d557604b
Usman9889 <usman9889a@gmail.com>
2026-09-02 16:08:13 +0530

    Move all Zeros to the end of the array

--- Arrays/283.MoveZeroToEnd.cpp
+++ Arrays/283.MoveZeroToEnd.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//Brute Force Approach
+//Time Complexity: O(n)
+//Space Complexity: O(n)
 void BruteForce(vector<int> & arr){
       int n = arr.size();
       vector<int>temp;
@@ -23,7 +25,37 @@ void BruteForce(vector<int> & arr){
             cout << arr[i] << " ";
       }   
 }
+//Optimal Approach
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+void Optimal(vector<int> & arr){
+      int n = arr.size();
+      int j = -1;
+      // Find the first zero element
+      for(int i=0; i<n; i++){
+            if(arr[i] == 0){
+                  j = i;
+                  break;
+            }
+      }
+      // If no zero element is found, return
+      if(j == -1){
+            return;
+      }
+      // Move all non-zero elements to the left
+      for(int i=j+1; i<n; i++){
+            if(arr[i] != 0){
+                  swap(arr[i], arr[j]);
+                  j++;
+            }
+      }
+      // Print the modified array
+      for(int i=0; i<n; i++){ 
+            cout << arr[i] << " ";
+      }
+}
 int main() {
       vector<int> arr = {0, 1, 0, 3, 12};
       BruteForce(arr);
+      Optimal(arr);
 }
