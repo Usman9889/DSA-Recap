6688e4ae0084953c61dc3d759bec2d76ffe7c443
Usman9889 <usman9889a@gmail.com>
2026-09-04 16:27:35 +0530

    Max Consecutive Ones

---
+++ Arrays/485.MaxConsecutiveOnes.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+      int n;
+      cin >> n;
+      vector<int> arr(n);
+      for(int i=0; i<n; i++){
+            cin >> arr[i];
+      }
+      int cnt = 0;
+      int maxi = 0;
+      for(int i=0; i<n; i++){
+            if(arr[i] == 1){
+                  cnt++;
+                  maxi = max(maxi, cnt);
+            }
+            else{
+                  cnt = 0;
+            }
+      }
+      cout << maxi << endl;
+      return 0;
+}
