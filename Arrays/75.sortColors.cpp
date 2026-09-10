55dc3e7862d26075e6fb6dd4a10f2ff6bb12b4d3
Usman9889 <usman9889a@gmail.com>
2026-09-10 16:45:08 +0530

    Given an array nums consisting of only 0, 1, or 2. Sort the array in non-decreasing order

--- Arrays/75.sortColors.cpp
+++ Arrays/75.sortColors.cpp
@@ -1,66 +1,60 @@
 // Approaches:(For Brute Force use any sorting algorithm)
 // 2. Better
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> sortBrute(vector<int> &nums) {
+    int n = nums.size();
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
 
-int main() {
-    int n;
-    cin >> n;
-
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    int zero = 0, one = 0, two = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (a[i] == 0) zero++;
-        else if (a[i] == 1) one++;
-        else two++;
+    for(int i = 0; i < n; i++) {
+        if(nums[i] == 0) cnt0++;
+        else if(nums[i] == 1) cnt1++;
+        else cnt2++;
     }
 
-    for (int i = 0; i < zero; i++) {
-        a[i] = 0;
-    }
-    for (int i = zero; i < zero + one; i++) {
-        a[i] = 1;
-    }
-    for (int i = zero + one; i < n; i++) {
-        a[i] = 2;
-    }
-    for (int x: a)
-        cout << x << " ";
+    int index = 0;
+    while(cnt0--) nums[index++] = 0;
+    while(cnt1--) nums[index++] = 1;
+    while(cnt2--) nums[index++] = 2;
 
+    return nums;
 }
-// 3.Optimal
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+// Optimal: Dutch National Flag
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+vector <int> sortDutchNationalFlag(vector<int> &nums) {\
+    int n = nums.size();
     int low = 0, mid = 0, high = n - 1;
-
-    while (mid <= high)
-    {
-        if (a[mid] == 0) {
-            swap(a[mid], a[low]);
+    while(mid <= high) {
+        if(nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
             low++;
             mid++;
-        } else if (a[mid] == 1) {
+        }
+        else if(nums[mid] == 1) {
             mid++;
-        } else {
-            swap(a[mid], a[high]);
+        }
+        else {
+            swap(nums[mid], nums[high]);
             high--;
         }
     }
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
+    return nums;
+}
+int main() {
+    vector<int> nums = {0, 2, 1, 2, 0, 1};
 
+    nums = sortBrute(nums);
+    for(int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    nums = sortDutchNationalFlag(nums);
+    for(int x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
 }
